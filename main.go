f4767abff16df3ae694a216ae4263cb9c3a17294
Dennis Collective <dennis.collective@gmail.com>
2015-01-11 14:25:59 -0800

    adding concurrency in main

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/denniscollective/dragonfly.go/http"
+	"runtime"
 )
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	err := http.StartServer()
 	if err != nil {
 		fmt.Println(err)
