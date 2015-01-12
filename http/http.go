dea34ccc2e5b8fdd3d4169c6fc6ec9607cf4e15c
Dennis Collective <dennis.collective@gmail.com>
2015-01-11 22:35:29 -0800

    decorate appache logger from gorlla with benchmarks

--- http/http.go
+++ http/http.go
@@ -3,9 +3,12 @@ package http
 import (
 	"fmt"
 	"github.com/denniscollective/dragonfly.go/dragonfly"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"time"
 )
 
 func StartServer() error {
@@ -13,9 +16,24 @@ func StartServer() error {
 	r.HandleFunc("/favicon.ico", hollar)
 	r.HandleFunc("/{b64JobString}", serveFile)
 
-	err := http.ListenAndServe(":2345", r)
-
+	err := http.ListenAndServe(":2345", handle(r))
 	return err
+}
+
+type benchmarkHandler struct {
+	handler http.Handler
+}
+
+func (h benchmarkHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	t := time.Now()
+	h.handler.ServeHTTP(w, req)
+	fmt.Println(time.Now().Sub(t))
+}
+
+func handle(h http.Handler) http.Handler {
+
+	return benchmarkHandler{handlers.CombinedLoggingHandler(os.Stdout, h)}
+
 }
 
 func hollar(response http.ResponseWriter, r *http.Request) {
@@ -23,7 +41,6 @@ func hollar(response http.ResponseWriter, r *http.Request) {
 }
 
 func serveFile(response http.ResponseWriter, request *http.Request) {
-	fmt.Printf("%s %s %s", request.RemoteAddr, request.Method, request.URL)
 	response.Header().Set("Access-Control-Allow-Origin", "*")
 
 	vars := mux.Vars(request)
@@ -33,7 +50,6 @@ func serveFile(response http.ResponseWriter, request *http.Request) {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(file.Name())
 
 	data, _ := ioutil.ReadAll(file)
 	response.Write(data)
